5135fbd5c308c505165ee2f1cefccf13ab00a492
mudable <mudableshilpa@gmail.com>
2021-10-23 11:59:51 +0530

    Solution for 3 bacis core java function

---
+++ src/com/bz/basic_core_function/RemainderAndQuetient.java
@@ -0,0 +1,12 @@
+package com.bz.basic_core_function;
+
+public class RemainderAndQuetient {
+	public static void main(String[] args) {
+		int divident=10, divider=3;
+		int quetient = divident/divider;
+		 int remainder = divident%divider;
+		System.out.println("que="+quetient);
+		System.out.println("rem="+remainder);
+	}
+
+}
