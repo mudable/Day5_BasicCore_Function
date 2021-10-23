f11c0619f5064d2884572900c9acb24461b0d751
mudable <mudableshilpa@gmail.com>
2021-10-23 11:31:43 +0530

    Added WelCome message

---
+++ src/com/bz/basic_core_function/BasicCore_Function.java
@@ -0,0 +1,8 @@
+package com.bz.basic_core_function;
+
+public class BasicCore_Function {
+	public static void main(String args[]) {
+		System.out.println("wel come to basic core java function program");
+	}
+
+}
