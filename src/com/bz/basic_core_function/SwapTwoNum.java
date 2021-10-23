8ce91a43967c9cec48474f81795e8924d069d8e1
mudable <mudableshilpa@gmail.com>
2021-10-23 12:07:51 +0530

    Solution for 3 bacis core java function

---
+++ src/com/bz/basic_core_function/SwapTwoNum.java
@@ -0,0 +1,16 @@
+package com.bz.basic_core_function;
+
+public class SwapTwoNum {
+	public static void main(String[] args) {
+		int a=40;
+		int b=30;
+		int store;
+		store=a;
+		 a=b;
+		 b=store;
+		System.out.println("a is:"+a);
+		System.out.println("b is:"+b);
+	}
+
+}
+
